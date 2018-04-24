91b10aeb377992fde3a9e8fd2a5d0e3202bed351
Youngjun Na <youngjunna@Youngjuns-iMac.local>
2018-04-24 17:54:59 +0900

    fix encoding type

--- R/visualization.R
+++ R/visualization.R
@@ -1,6 +1,5 @@
 pacman::p_load("ggmap","dplyr","ggplot2","maps")
-farm <- read.csv("geomap.txt",stringsAsFactors = FALSE)
-
+farm <- read.csv("geomap.txt",stringsAsFactors = FALSE,fileEncoding = "EUC-KR")
 
 korea <- enc2utf8("South Korea") %>%
   geocode() %>%
@@ -14,7 +13,6 @@ korea + stat_density_2d(farm,aes(lon,lat,fill=..area..,alpha=..area..),geom="ras
 
 ggplot(farm, aes(lon,lat,group=group,fill=area)) + geom_polygon(colour="black") + coord_map("d")
 
-
 ggplot(korea, aes(lon,lat,group=group,)) + geom_polygon()+geom_raster(aes(fill=area),hjust=0.5,vjust=0.5,interpolate = FALSE) + scale_fill_gradientn(colours = rev(rainbow(7)), na.value = NA) + theme_bw() + coord_fixed(1.3)
 
 
@@ -24,7 +22,7 @@ ggplot(seals,aes(long,lat))+geom_tile(aes(fill=z))
 
 
 # manure data ####
-manure <- read.csv("manure.csv")
+manure <- read.csv("manure.csv",fileEncoding = "EUC-KR")
 manure
 
 seoul <- enc2utf8("대한민국") %>%
