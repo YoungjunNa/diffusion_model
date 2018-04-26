b6178c0c823793e9c7f0ebf03587ccf9471f80f0
Youngjun Na <youngjunna@Youngjuns-iMac.local>
2018-04-26 11:39:33 +0900

    1st success for visualization

--- R/get_geomap.R
+++ R/get_geomap.R
@@ -17,45 +17,18 @@ shape <- readShapePoly("/Users/youngjunna/Github/diffusion_model/R/sig_shp")
 
 file.choose()
 
-
 # farm data ####
 farm <- read.csv("farmdata.txt",stringsAsFactors = FALSE,fileEncoding = "EUC-KR")
 farm <- filter(farm, 영업상태=="정상")
 farm <- filter(farm, 축종=="한우" | 축종=="젖소" | 축종=="육우")
 result <- group_by(farm,시군구) %>% summarise(area=sum(규모))
 
-# map1 <- korpop1
+animal <- read.csv("animal-2016.txt",stringsAsFactors = FALSE)
+ruminant <- filter(animal, 축종명=="한우"|축종명=="젖소"|축종명=="육우")
+result <- group_by(ruminant,시군) %>% summarise(animal=sum(규모))
+result$시군 <- as.factor(result$시군)
+
 map2 <- korpop2
-# map3 <- korpop3
 
-result$시군구
-
-
-
-
-
-# 위도경도 function ####
-lon <- function(name){
-  map <- enc2utf8(name) %>%
-    geocode()
-  print(map$lon)
-}
-
-lat <- function(name){
-  map <- enc2utf8(name) %>%
-    geocode()
-  print(map$lat)
-}
-
-result$lon <- NA
-result$lat <- NA
-
-
-for(i in 1:(nrow(result))){
-  result$lon[i] <- lon(result$시군구[i])
-  result$lat[i] <- lat(result$시군구[i])
-  i+1
-}
-
-
-write.csv(result, "geomap.txt",row.names=FALSE)
+names(map2)[2] <- "시군"
+test <- merge(map2, result,by="시군")
