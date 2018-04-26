fe0753db2f9233617da97358dd151e0f61bfd0d2
Youngjun Na <youngjunna@Youngjuns-iMac.local>
2018-04-26 15:23:20 +0900

    add manure data

--- R/get_geomap.R
+++ R/get_geomap.R
@@ -1,34 +1,60 @@
-pacman::p_load("ggmap","dplyr","kormaps2014","sp","maptools","rgdal")
+pacman::p_load("dplyr","kormaps2014")
 register_google(key = "your API key from https://developers.google.com/maps/documentation/geocoding/get-api-key")
 
-# sph file read
-shape <- readOGR(dsn = path.expand("/Users/youngjunna/Github/diffusion_model/R"),layer="sig_shp")
-shape <- readOGR(dsn = path.expand("sig_shp.shp"), 
-                 layer = "sig_shp")
-library(raster)
-shp <- shapefile("/Users/youngjunna/Github/diffusion_model/R/sig_shp_all.shp")
+# 가축분뇨 배출원단위(kg/두/일)
+hanwoo <- data.frame(bw=350,feces=8,urine=5.7,total=13.7,output=13.7)
+holstein <- data.frame(bw=450,feces=19.2,urine=10.9,total=30.1,output=37.7)
+swine <- data.frame(bw=60,feces=0.87,urine=1.74,total=2.49,output=5.1)
+layer <- data.frame(bw=1.7,feces=0.1247,urine=NA,total=0.1247,output=0.1247)
+broiler <- data.frame(bw=1.3,feces=0.0855,urine=NA,total=0.0855,output=0.0855)
 
-library(sf)
-shp <- st_read(system.file("/Users/youngjunna/Github/diffusion_model/R/sig_shp_all.shp", package="sf"))
+manure_out <- list(hanwoo=hanwoo,holstein=holstein,swine=swine,layer=layer,broiler=broiler)
 
-# shape <- read_sf(dsn = ".", layer = "sig_shp_all")
-# shape <- shapefile("sig_shp_all")
-shape <- readShapePoly("/Users/youngjunna/Github/diffusion_model/R/sig_shp")
-
-file.choose()
-
-# farm data ####
-farm <- read.csv("farmdata.txt",stringsAsFactors = FALSE,fileEncoding = "EUC-KR")
-farm <- filter(farm, 영업상태=="정상")
-farm <- filter(farm, 축종=="한우" | 축종=="젖소" | 축종=="육우")
-result <- group_by(farm,시군구) %>% summarise(area=sum(규모))
-
+# 시군기준
 animal <- read.csv("animal-2016.txt",stringsAsFactors = FALSE)
 ruminant <- filter(animal, 축종명=="한우"|축종명=="젖소"|축종명=="육우")
 result <- group_by(ruminant,시군) %>% summarise(animal=sum(규모))
-result$시군 <- as.factor(result$시군)
+# result <- group_by(animal, 시군) %>% summarise(animal=sum(규모))
+
+result$output <- NA
+result$output <- ifelse(ruminant$축종명=="한우",ruminant$규모*manure_out$hanwoo$output)
+
 
 map2 <- korpop2
-
 names(map2)[2] <- "시군"
-test <- merge(map2, result,by="시군")
+map2$시군 <- as.character(map2$시군)
+map2$시군 <- gsub(" ","",map2$시군)
+
+merge <- merge(map2,result,by="시군",all=TRUE)
+merge$animal[is.na(merge$animal)] <- 0
+
+write.csv(merge,"geomap.txt",row.names = FALSE)
+
+# 읍면동 기준
+animal <- read.csv("animal-2016.txt",stringsAsFactors = FALSE)
+ruminant <- filter(animal, 축종명=="한우"|축종명=="젖소"|축종명=="육우")
+result <- group_by(ruminant,읍면동) %>% summarise(animal=sum(규모))
+
+map3 <- korpop3
+names(map3)[2] <- "읍면동"
+map3$읍면동 <- gsub(" ","",map3$읍면동)
+
+merge <- merge(map3,result,by="읍면동",all=TRUE)
+is.na(merge$animal) <- 0
+
+# 가축분뇨 처리
+manure <- read.csv("manure.csv",fileEncoding = "EUC-KR")
+# manure <- group_by(manure,행정.시군코드명) %>% summarise(manure=sum(가축원뇨수거.원뇨수거.톤.일.))
+manure <- group_by(manure,행정.시군코드명) %>% summarise(manure=sum(가축원뇨수거.수거물량.통.))
+names(manure)[1] <- "시군"
+
+map2 <- korpop2
+names(map2)[2] <- "시군"
+map2$시군 <- as.character(map2$시군)
+map2$시군 <- gsub(" ","",map2$시군)
+
+merge <- merge(map2,manure,by="시군",all=TRUE)
+merge$manure[is.na(merge$manure)] <- 0
+merge$manure[merge$시군=="양산시"] <- 0
+merge$manure[merge$시군=="함안군"] <- 0
+merge$manure[merge$시군=="사천시"] <- 0
--- R/visualization.R
+++ R/visualization.R
@@ -1,54 +1,26 @@
-pacman::p_load("ggmap","dplyr","ggplot2","maps","kormaps2014")
-farm <- read.csv("geomap.txt",stringsAsFactors = FALSE,fileEncoding = "EUC-KR")
-
-korea <- enc2utf8("South Korea") %>%
-  geocode() %>%
-  as.numeric() %>%
-  get_googlemap(maptype="roadmap",zoom=7) %>%
-  ggmap()
+pacman::p_load("ggmap","dplyr","ggplot2","maps","kormaps2014","plotly")
+farm <- read.csv("geomap.txt",stringsAsFactors = FALSE)
 
 # 시각화
 theme_set(theme_gray(base_family="NanumGothic"))
 
-ggplot(korpop3,aes(map_id=code,fill=주택_계_호))+
+ggplot(farm,aes(map_id=code,fill=animal))+
+  geom_map(map=kormap2,colour="black",size=0.1)+
+  expand_limits(x=kormap2$long,y=kormap2$lat)+
+  scale_fill_gradientn(colours=c('white','orange','red'))+
+  ggtitle("2016 대가축 분포")+
+  coord_map()
+
+ggplot(merge,aes(map_id=code,fill=animal))+
   geom_map(map=kormap3,colour="black",size=0.1)+
   expand_limits(x=kormap3$long,y=kormap3$lat)+
   scale_fill_gradientn(colours=c('white','orange','red'))+
-  ggtitle("2015년도 시도별 인구분포도")+
+  ggtitle("2016 대가축 분포")+
   coord_map()
 
-map1 <- korpop1
-map <- korpop2
-
-korea + stat_density_2d(farm,aes(lon,lat,fill=..area..,alpha=..area..),geom="raster",size=2,bins=30)
-
-ggplot(farm,aes(lon,lat))+geom_raster(aes(fill=area),hjust=0.5,vjust=0.5,interpolate=FALSE)
-
-ggplot(farm, aes(lon,lat,group=group,fill=area)) + geom_polygon(colour="black") + coord_map("d")
-
-ggplot(korea, aes(lon,lat,group=group,)) + geom_polygon()+geom_raster(aes(fill=area),hjust=0.5,vjust=0.5,interpolate = FALSE) + scale_fill_gradientn(colours = rev(rainbow(7)), na.value = NA) + theme_bw() + coord_fixed(1.3)
-
-
-seals$z <- with(seals, sqrt(delta_long^2+delta_lat^2))
-ggplot(seals,aes(long,lat))+geom_raster(aes(fill=z),hjust=0.5,vjust=0.5,interpolate = FALSE)
-ggplot(seals,aes(long,lat))+geom_tile(aes(fill=z))
-
-
-# manure data ####
-manure <- read.csv("manure.csv",fileEncoding = "EUC-KR")
-manure
-
-seoul <- enc2utf8("대한민국") %>%
-  geocode() %>%
-  as.numeric() %>%
-  get_googlemap(maptype="roadmap",zoom=12) %>%
-  ggmap()
-
-korea <- enc2utf8("South Korea") %>%
-  geocode() %>%
-  as.numeric() %>%
-  get_googlemap(maptype="roadmap",zoom=7) %>%
-  ggmap()
-
-korea + geom_map(farm,aes(lon,lat,group=group,fill=area))
-
+ggplot(merge,aes(map_id=code,fill=manure))+
+  geom_map(map=kormap2,colour="black",size=0.1)+
+  expand_limits(x=kormap2$long,y=kormap2$lat)+
+  scale_fill_gradientn(colours=c('white','blue'))+
+  ggtitle("2016 분뇨 처리량")+
+  coord_map()
